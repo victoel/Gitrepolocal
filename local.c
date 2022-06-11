c6143ab7ba4900bed14e80134b758c56f3fea366
Victor Iwegbulem <chukwuemekavictor19@gmail.com>
2022-06-11 21:23:22 +0100

    New branch update

--- local.c
+++ local.c
@@ -3,6 +3,5 @@ int main()
 {
   printf("Hello Africa\n");
   printf("Hello from feature-code1\n");
-    printf("Hello from feature-code2\n");
-    printf("Hello from feature-code3\n");
+
 }
---
+++ local.md
@@ -0,0 +1,4 @@
+This is a local repo on my computer
+
+Update 1
+updated 2
